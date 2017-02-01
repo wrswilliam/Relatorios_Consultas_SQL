36d08c91151b45d280554a53f2716965e9f61452
wrswilliam <william.silva@grupoaugustocury.com.br>
2017-02-01 13:49:36 -0200

    Add files via upload
    Verificar quais processos estão ativos no banco no momento da execução!!!!

---
+++ querys_ativo_banco.sql
@@ -0,0 +1,14 @@
+SELECT
+	Processo      = spid
+	,Computador   = hostname
+	,Usuario      = loginame
+	,Status       = status
+	,BloqueadoPor = blocked
+	,TipoComando  = cmd
+	,Aplicativo   = program_name
+FROM
+	master..sysprocesses
+WHERE
+	status in ('runnable', 'suspended')
+ORDER BY
+	blocked desc, status, spid
