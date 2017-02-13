280abcfff607c16ab6c87fa0f68cdecee66f1e8e
wrswilliam <william.silva@grupoaugustocury.com.br>
2017-02-13 14:27:52 -0200

    Melhorias no insert de pedido
    Foi criado um laço de condição para verificar e controlar a data tanto
    quanto o pedido é feito pelo portal e pelo sap

--- Transactions_Travas_Pedidos_Compras.sql
+++ Transactions_Travas_Pedidos_Compras.sql
@@ -1,20 +1,37 @@
 --- Verifica data de compra viagem
-
-IF @object_type ='22' AND @transaction_type = ('A')
-	BEGIN
-		IF(
-			(SELECT COUNT(*) FROM OPOR T0 INNER JOIN POR1 T1 ON T0.DocEntry = T1.DocEntry
-				WHERE T0.DocEntry = @list_of_cols_val_tab_del
-					  AND (convert(date,t1.U_LGO_Data) <> convert(date,GETDATE())
-					  AND T1.ITEMCODE IN ('CO00082','CO01233')))>0 )
 	
-
+IF @object_type ='22' AND @transaction_type in ('A','U')
 	BEGIN
-			SET @error = 2525
-			SET @error_message = 'O PEDIDO DE VIAGEM TEM QUE SER FEITO NO DIA ATUAL'
+	IF(
+		(SELECT COUNT(*) FROM OPOR T0 INNER JOIN POR1 T1 ON T0.DocEntry = T1.DocEntry
+						 WHERE T0.DocEntry = @list_of_cols_val_tab_del
+						 AND T1.U_LGO_Data IS NOT NULL) > 0)
+		  BEGIN
 
-	end
-	end
+				IF(
+					(SELECT COUNT(*) FROM OPOR T0 INNER JOIN POR1 T1 ON T0.DocEntry = T1.DocEntry
+									 WHERE T0.DocEntry = @list_of_cols_val_tab_del
+									 AND CONVERT (DATE,T1.U_LGO_DATA) <> CONVERT (DATE,GETDATE())
+									 AND T1.ItemCode IN ('CO00082','CO01233'))>0)
+					BEGIN 
+
+						SET @error = 2525
+						SET @error_message ='O PEDIDO DE VIAGEM TEM QUE SER FEITO NO DIA ATUAL'
+					END
+			END
+	 ELSE IF(
+			
+				(SELECT COUNT(*) FROM OPOR T0 INNER JOIN POR1 T1 ON T0.DocEntry = T1.DocEntry
+									 WHERE T0.DocEntry = @list_of_cols_val_tab_del
+									 AND CONVERT (DATE,T0.DocDate) <> CONVERT (DATE,GETDATE())
+									 AND T1.ItemCode IN ('CO00082','CO01233'))>0)
+					BEGIN 
+
+						SET @error = 2525
+						SET @error_message = 'O PEDIDO DE VIAGEM TEM QUE SER FEITO NO DIA ATUAL'
+					END
+
+END
 
 ---Verifica data de entrega está no prazo
 
