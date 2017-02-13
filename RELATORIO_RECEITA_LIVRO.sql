27b9605667098b4b747426197aa719fc36f5b70d
wrswilliam <william.silva@grupoaugustocury.com.br>
2017-02-13 16:54:00 -0200

    RELATORIO_RECEITA_LIVRO
    RELATORIO QUE TRAS N° PRIMARIO, N° DA NOTA, ITEM, CÓDIGO IMPOSTO, CFOP,
    PREÇO -- AMANDA FINANCEIRO

---
+++ RELATORIO_RECEITA_LIVRO.sql
@@ -0,0 +1,13 @@
+SELECT 
+		 T0.DocEntry
+		,T0.SERIAL AS 'N° NOTA'
+		,T1.ItemCode
+		,T1.TaxCode
+		,T1.CFOPCODE
+		,CAST((T1.PRICE * T1.QUANTITY)AS DECIMAL(10,2))AS 'PREÇO'
+	 FROM OINV T0
+    INNER JOIN INV1 T1 ON T0.DocEntry = T1.DocEntry
+	WHERE T0.DocDate BETWEEN [%0] AND [%1]
+		AND (T1.CFOPCODE = [%2] OR T1.CFOPCODE =[%3])
+
+
