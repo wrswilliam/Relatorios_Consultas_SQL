855afd95bc39f5209657846ccbb2ecd67a283d31
wrswilliam <william.silva@grupoaugustocury.com.br>
2017-02-14 15:46:47 -0200

    Correção nota sap 2252620
    Correção de  nota sap 2252620 - Verificação de integridade falhou;
    inconsistencias de multiplas filiais

---
+++ nota sap 2252620.sql
@@ -0,0 +1,26 @@
+SELECT   T0.empID
+FROM  OHEM T0
+INNER JOIN OUSR T1
+ON  T1.USERID = T0.userId
+LEFT OUTER JOIN USR6 T2
+ON   T2.BPLId = T0.BPLId
+AND   T1.USER_CODE = T2.UserCode
+WHERE    T2.BPLId IS NULL
+AND   T0.BPLId> 0
+
+
+
+UPDATE  OHEM
+
+SET            BPLId = null
+ WHERE   empID IN
+(      SELECT  T0.empID
+       FROM    OHEM T0
+       INNER JOIN OUSR T1
+              ON      T1.USERID = T0.userId
+       LEFT OUTER JOIN USR6 T2
+              ON      T2.BPLId = T0.BPLId
+              AND  T1.USER_CODE = T2.UserCode
+       WHERE   T2.BPLId IS NULL
+
+              AND  T0.BPLId> 0)
