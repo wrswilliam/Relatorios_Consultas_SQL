b1e861cc7cbff3c0a604c63ee839a0560f1f611a
William Rodrigues da Silva <wrs.william1@gmail.com>
2017-07-31 14:09:31 -0300

    Atividades_tableu
    Select, considerando o attendetuser

---
+++ Atividades_tableau.sql
@@ -0,0 +1,48 @@
+SELECT DISTINCT
+	t0.Closed,
+	T6.CardFName,
+	t0.ClgCode,                                                                              
+	CAST(t2.U_CodPerg as nvarchar(max))[CodPerg],                                                   
+	CAST(U_CodAtiv as nvarchar(max))[CodAtiv], 
+	T4.NAME AS Assunto,                                                     
+	CAST(U_Pergunta as nvarchar(max))[Perg],                                                        
+	CAST(U_CodAssunto as nvarchar(max))[Ass],	                                                    
+	CAST(U_Resposta as nvarchar(max))[Resp],                                                        
+	CAST(T0.CardCode as nvarchar(max))[CardCode],                                                      
+	CAST(AttendUser as nvarchar(max))[AttendUser],  
+	T5.U_NAME,
+	t0.U_LGO_PARC_IMPLE                                               
+FROM OCLG t0                                                                                    
+	INNER JOIN [@LGCCLASAT] T2 ON T2.U_CodAtiv = T0.ClgCode                                         
+	INNER join [@LGLCCLASAT] T3 ON t2.code = t3.code 
+	INNER JOIN OCLS T4 ON T4.CODE = T0.CntctSbjct
+	INNER JOIN OUSR T5 ON T0.AttendUser = T5.USERID
+	INNER JOIN OCRD T6 ON T0.CardCode = T6.CardCode
+                                               
+WHERE t3.u_checado = 'SIM'                                                                      
+  AND T0.inactive = 'N'             
+
+
+  union all
+
+  SELECT DISTINCT
+    t0.Closed,
+	T6.CardFName,
+	t0.ClgCode,                                                                              
+	CodPerg ='',                                                   
+	CodAtiv ='', 
+	T4.NAME AS Assunto,                                                     
+	Perg ='',                                                        
+	Ass ='',	                                                    
+	Resp ='',                                                        
+    T0.CardCode,                                                      
+	t0.AttendUser ,  
+	T5.U_NAME,
+	t0.U_LGO_PARC_IMPLE                                               
+FROM OCLG t0                                                                                    
+	INNER JOIN OCLS T4 ON T4.CODE = T0.CntctSbjct
+	INNER JOIN OUSR T5 ON T0.AttendUser = T5.USERID
+	INNER JOIN OCRD T6 ON T0.CardCode = T6.CardCode
+                                               
+WHERE CLOSED = 'N'                                                             
+  AND T0.inactive = 'N'
